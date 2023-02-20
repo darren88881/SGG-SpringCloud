62078dbe9e5948c66690b4e04f85902dfc8699a7
darren88881 <darren88881@outlook.com>
2023-02-20 15:47:38 +0800

    SGG-springcloud-CloudConsumerOrder80

---
+++ cloud-consumer-order80/src/main/java/com/itguigu/springcloud/CloudConsumerOrder80.java
@@ -0,0 +1,16 @@
+package com.itguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @Author darren
+ * @Date 2023/2/20 15:00
+ */
+@SpringBootApplication
+public class CloudConsumerOrder80 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConsumerOrder80.class, args);
+    }
+}
