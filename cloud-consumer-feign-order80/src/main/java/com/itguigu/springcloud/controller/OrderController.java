52303802bbebb93cfd31f24dceb8e3fe01ded517
darren88881 <darren88881@outlook.com>
2023-02-24 14:16:28 +0800

    SGG-springcloud-使用brbbon设置超时时间

--- cloud-consumer-feign-order80/src/main/java/com/itguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-feign-order80/src/main/java/com/itguigu/springcloud/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.itguigu.springcloud.entities.Payment;
 import com.itguigu.springcloud.service.PaymentFeignService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
@@ -14,14 +15,20 @@ import javax.annotation.Resource;
  * @Date 2023/2/20 15:01
  */
 @RestController
+@RequestMapping("/consumer/feign/payment")
 public class OrderController {
 
     @Resource
     private PaymentFeignService paymentFeignService;
 
-    @GetMapping("/consumer/feign/payment/getPaymentById/{id}")
+    @GetMapping("/getPaymentById/{id}")
     public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
         return paymentFeignService.getPaymentById(id);
     }
 
+
+    @GetMapping("/timeout")
+    String getPaymentTimeout(){
+        return paymentFeignService.getPaymentTimeout();
+    }
 }
--- cloud-consumer-feign-order80/src/main/java/com/itguigu/springcloud/service/PaymentFeignService.java
+++ cloud-consumer-feign-order80/src/main/java/com/itguigu/springcloud/service/PaymentFeignService.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  *
@@ -18,4 +19,7 @@ public interface PaymentFeignService {
 
     @GetMapping("/payment/getPaymentById/{id}")
     CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+
+    @GetMapping("/payment/timeout")
+    String getPaymentTimeout();
 }
