874f0c8045b64cac145e15cdd341f222980ff0b7
darren88881 <darren88881@outlook.com>
2023-02-28 12:12:36 +0800

    SGG-springcloud-openFeign是一个HTTP客户端，映射来请求转发。

--- cloud-consumer-feign-order80/src/main/java/com/itguigu/springcloud/CloudConsumerFeignOrder80.java
+++ cloud-consumer-feign-order80/src/main/java/com/itguigu/springcloud/CloudConsumerFeignOrder80.java
@@ -6,6 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
+ * 1、openFeign是一个HTTP客户端，它融合了springmvc的注解，使之可以用REST风格的映射来请求转发。
+ *
+ * 2、可以把openFegin理解为是controller层或是service层。可以取代springmvc控制层作为请求映射，
+ *    亦或是作为service层处理逻辑，只不过这里，openFeign只是做一个请求转发的逻辑操作。
+ *
+ * 3、openFeign整合了hystrix做熔断处理，同时，可以和ribbon客户端负载均衡、Eureka注册中心配合使用，
+ *    实现负载均衡的客户端。
+ *
+ * 4、openFeign有一个很重要的功能：fallback，其实它是hystrix的特性
+ *
  *
  * @Author darren
  * @Date 2023/2/20 15:00
--- cloud-consumer-feign-order80/src/main/java/com/itguigu/springcloud/service/PaymentFeignService.java
+++ cloud-consumer-feign-order80/src/main/java/com/itguigu/springcloud/service/PaymentFeignService.java
@@ -6,10 +6,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  *
+ * 2、可以把openFegin理解为是controller层或是service层。可以取代springmvc控制层作为请求映射，
+ *   亦或是作为service层处理逻辑，只不过这里，openFeign只是做一个请求转发的逻辑操作。
  * @Author darren
  * @Date 2023/2/24 13:26
  */
