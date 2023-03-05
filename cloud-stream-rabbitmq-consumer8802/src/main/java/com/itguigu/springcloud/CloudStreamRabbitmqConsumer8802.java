8e1198f9272c852288c3857ac83d71c27bdf856d
darren88881 <darren88881@outlook.com>
2023-03-05 11:10:29 +0800

    SGG-springcloud-cloud-stream-consumer接受发来的消息

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/itguigu/springcloud/CloudStreamRabbitmqConsumer8802.java
@@ -0,0 +1,16 @@
+package com.itguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @Author darren
+ * @Date 2023/3/5 11:04
+ */
+@SpringBootApplication
+public class CloudStreamRabbitmqConsumer8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStreamRabbitmqConsumer8802.class, args);
+    }
+}
