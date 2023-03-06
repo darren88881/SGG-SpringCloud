ec5777a5051096cf7237c348226896014fa79f98
darren88881 <darren88881@outlook.com>
2023-03-06 20:48:59 +0800

    SGG-springcloud-cloud-nacos做注册中心

---
+++ cloudalibaba-nacos-provider-payment9001/src/main/java/com/itguigu/springcloud/CloudalibabaProviderPayment9001.java
@@ -0,0 +1,20 @@
+package com.itguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @EnableDiscoveryClient 该注解用于向使用consul或者zookeeper作为注册中心时注册服务
+ *
+ * @Author darren
+ * @Date 2023/3/6 17:39
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CloudalibabaProviderPayment9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudalibabaProviderPayment9001.class,args);
+    }
+}
