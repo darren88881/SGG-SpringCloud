938462c73b6e31e987fbbc3b4a4e9470deb0b2c6
darren88881 <darren88881@outlook.com>
2023-03-08 18:18:06 +0800

    SGG-springcloud-cloud-Gateway服务端服务名称修改

--- cloud-gateway-gateway6001/src/main/java/com/itguigu/springcloud/CloudGateway6001.java
+++ cloud-gateway-gateway6001/src/main/java/com/itguigu/springcloud/CloudGateway6001.java
@@ -11,8 +11,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
-public class CloudGateway9527 {
+public class CloudGateway6001 {
     public static void main(String[] args) {
-        SpringApplication.run(CloudGateway9527.class, args);
+        SpringApplication.run(CloudGateway6001.class, args);
     }
 }
