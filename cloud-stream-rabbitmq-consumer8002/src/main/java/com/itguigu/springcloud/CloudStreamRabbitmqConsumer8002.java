1491ae3713bc75dc81dd93b2c1cacb63d2dabeea
darren88881 <darren88881@outlook.com>
2023-03-08 18:58:12 +0800

    SGG-springcloud-cloud-Stream服务端服务名称修改

---
+++ cloud-stream-rabbitmq-consumer8002/src/main/java/com/itguigu/springcloud/CloudStreamRabbitmqConsumer8002.java
@@ -0,0 +1,16 @@
+package com.itguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @Author darren
+ * @Date 2023/3/5 11:04
+ */
+@SpringBootApplication
+public class CloudStreamRabbitmqConsumer8002 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStreamRabbitmqConsumer8002.class, args);
+    }
+}
---
+++ cloud-stream-rabbitmq-provider8001/src/main/java/com/itguigu/springcloud/CloudStreamRabbitmqProvider8001.java
@@ -0,0 +1,16 @@
+package com.itguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @Author darren
+ * @Date 2023/3/5 10:20
+ */
+@SpringBootApplication
+public class CloudStreamRabbitmqProvider8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStreamRabbitmqProvider8001.class, args);
+    }
+}
